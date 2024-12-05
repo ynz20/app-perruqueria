87e0ebcd74326980f4bc16334bd155df45d9378d
Yasser <yasser.zuaki@cirvianum.cat>
2024-12-05 18:57:31 +0100

    First commit

---
+++ database/migrations/2024_12_04_170433_modify_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+             //Eliminem els camps innecesaris
+            $table->dropColumn(['email', 'email_verified_at', 'password', 'remember_token', 'created_at', 'updated_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
